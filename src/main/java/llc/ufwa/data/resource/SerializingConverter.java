26bbfda33056d9d51284c84374454278c7f10627
kh14821 <kh14821@localhost>
2012-12-03 10:42:42 -0700

    Added cast to resolve failing Jenkins build (Java compiler bug). Added @SuppressWarnings("unchecked") to suppress warning related to cast.

--- src/main/java/llc/ufwa/data/resource/SerializingConverter.java
+++ src/main/java/llc/ufwa/data/resource/SerializingConverter.java
@@ -20,10 +20,11 @@ public class SerializingConverter<Value> implements Converter<Value, byte []> {
     }
 
     @Override
+	@SuppressWarnings("unchecked")  // Supresses warning from unchecked cast.  Cast was added to fix error in Jenkins build environment.
     public Value restore(byte[] newVal) throws ResourceException {
         
         try {
-            return DataUtils.deserialize(newVal);
+            return (Value) DataUtils.deserialize(newVal);
         }
         catch (IOException e) {
             throw new ResourceException(e);
