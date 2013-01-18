b899b0c38a8e852951c1df6b418586b540fefd5e
mr mrporter <mr.mrporter@gmail.com>
2013-01-18 15:34:21 -0700

    added exception

--- src/main/java/llc/ufwa/data/resource/provider/ResourceProvider.java
+++ src/main/java/llc/ufwa/data/resource/provider/ResourceProvider.java
@@ -1,11 +1,13 @@
 package llc.ufwa.data.resource.provider;
 
+import java.net.SocketException;
+
 import llc.ufwa.data.exception.ResourceException;
 
 
 public interface ResourceProvider<TValue> {
 	
 	boolean exists() throws ResourceException;
-	TValue provide() throws ResourceException; 
+	TValue provide() throws ResourceException, SocketException; 
 	
 }
