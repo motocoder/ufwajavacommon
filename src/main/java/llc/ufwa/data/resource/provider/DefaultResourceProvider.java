971fbbd898b5cdf6d2af7a9a2552522e5b51f678
mr mrporter <mr.mrporter@gmail.com>
2013-01-18 16:09:14 -0700

    added exception handling

--- src/main/java/llc/ufwa/data/resource/provider/DefaultResourceProvider.java
+++ src/main/java/llc/ufwa/data/resource/provider/DefaultResourceProvider.java
@@ -1,11 +1,13 @@
 package llc.ufwa.data.resource.provider;
 
+import java.net.SocketException;
+
 import llc.ufwa.data.exception.ResourceException;
 
 public abstract class DefaultResourceProvider<Value> implements ResourceProvider<Value> {
 
     @Override
-    public boolean exists() throws ResourceException {
+    public boolean exists() throws ResourceException, SocketException {
         return provide() != null;
     }
     
