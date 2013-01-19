578444014e98f6a653aca539631f17b01ee21461
sean.robert.wagner <sean.robert.wagner@localhost>
2013-01-18 18:52:47 -0700

    Tards will be tards

--- src/main/java/llc/ufwa/data/resource/provider/ResourceProvider.java
+++ src/main/java/llc/ufwa/data/resource/provider/ResourceProvider.java
@@ -1,13 +1,11 @@
 package llc.ufwa.data.resource.provider;
 
-import java.net.SocketException;
-
 import llc.ufwa.data.exception.ResourceException;
 
 
 public interface ResourceProvider<TValue> {
 	
-	boolean exists() throws ResourceException, SocketException;
-	TValue provide() throws ResourceException, SocketException; 
+	boolean exists() throws ResourceException;
+	TValue provide() throws ResourceException; 
 	
 }
