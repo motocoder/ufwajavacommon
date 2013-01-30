aec01b7bba7d679a3eac4b282be5e6fecd755fff
mr mrporter <mr.mrporter@gmail.com>
2013-01-30 15:27:13 -0700

    complete rewrite

--- src/main/java/llc/ufwa/concurrency/OneThroughLimitingExecutor.java
+++ src/main/java/llc/ufwa/concurrency/OneThroughLimitingExecutor.java
@@ -1,91 +1,114 @@
 package llc.ufwa.concurrency;
 
+
+
+import java.util.LinkedList;
 import java.util.concurrent.Executor;
 
+import llc.ufwa.javacommon.test.concurrency.OneThroughLimitingExecutorTest;
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OneThroughLimitingExecutor implements Executor {
-     
-    private final Object lock = new Object();
-    private final LimitingExecutorService limiting;
+	
+	private static final Logger logger = LoggerFactory.getLogger(OneThroughLimitingExecutorTest.class);	
+
     private final Executor threads;
-    
-    private volatile boolean running;
+    private final LinkedList<Runnable> runList = new LinkedList<Runnable>();
     
     public OneThroughLimitingExecutor(
-        final Executor executor,
-        final LimitingExecutorService limited
+        final Executor executor
     ) {
         
-        this.limiting = limited;
         this.threads = executor;
         
     }
-
-    @Override
-    public void execute(final Runnable command) {
-        
-        //limit one waiting
-        limiting.execute(
-                
-            new Runnable() {
-
-                @Override
-                public void run() {
+    
+	private void startIfAvailable() {
+		
+		if(runList.size() > 0) {
+			
+			threads.execute(
+					
+			    new Runnable() {
+	
+					@Override
+					public void run() {
+	
+					    final Runnable toRun;
                         
-                    synchronized(lock) {
-                        
-                        while(running) {
+                        synchronized(runList) {
                             
-                            try {
-                                lock.wait(100);
+                            if(runList.size() != 0) {
+                                toRun = runList.getFirst();
                             }
-                            catch(InterruptedException e) {
-                                return;
+                            else {
+                                toRun = null;
                             }
                             
                         }
-                        
-                        threads.execute(
-                                
-                            new Runnable() {
-
-                                @Override
-                                public void run() {
-                                    
-                                    
-                                    
-                                    try {
-                                        
-                                        command.run();
-                                        
-                                    }
-                                    finally {
-                                        
-                                        synchronized(lock) {
-                                            
-                                            running = false;
-                                        
-                                            lock.notify();
-                                            
-                                        }
-                                        
-                                    }
-                                    
-                                }
-                                
-                            }
+					    
+                        if(toRun != null) {
                             
-                        );
-                        
-                        running = true;
-                        
-                    }
-                                    
-                }
-                
-            }
-            
-        );
-        
-    }
+    						try {
+    						    
+    						    toRun.run();
+    							
+    							logger.debug("if runList.size() >0  push and call startIfAvailable just ran");
+    							
+    						} 
+    						finally {
+    						    
+    							logger.debug("finally rerun startIfAvailable after pop run... before");
+    							runList.removeFirst();
+    							logger.debug("finally rerun startIfAvailable after pop run... after");
+    							
+    							startIfAvailable();
+    							
+    						}
+    						
+                        }
+	
+					}
+					
+				}
+			    
+			);
+			
+		}
+		
+	}
 
+    @Override
+	public void execute(final Runnable command) {
+
+		synchronized (runList) {
+			if (runList.size() == 0) {
+
+				runList.add(command);
+				startIfAvailable();
+				logger.debug("if runList.size() == 0 push and call to startIfAvailable just ran");
+
+			}
+			else if (runList.size() == 1) {
+
+				runList.add(command);
+				startIfAvailable();
+				logger.debug("if runList.size() == 1 add to end of list and call to theQueue() just ran");
+
+			}
+			else if (runList.size() == 2) {
+
+				runList.removeLast();
+				runList.add(command);
+				startIfAvailable();
+				logger.debug("if runList.size() == 2 remove last item in list and add to end of list and call to theQueue() just ran");
+
+			}
+		}
+
+	}
+    
 }
