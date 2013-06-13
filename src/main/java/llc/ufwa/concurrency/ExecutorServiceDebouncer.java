e7eb1924daf9214321ecffad4951d55162e4c51a
chase <chase@localhost>
2013-06-13 16:15:11 -0700

    added ExecutorServiceDebouncer, modified DebouncerTest to ignore old Debouncer and instead only test ExecutorServiceDebouncer

---
+++ src/main/java/llc/ufwa/concurrency/ExecutorServiceDebouncer.java
@@ -0,0 +1,266 @@
+package llc.ufwa.concurrency;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import llc.ufwa.data.exception.ResourceException;
+import llc.ufwa.data.resource.provider.PushProvider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class basically works like a debouncer. If lots of calls are being 
+ * made to something it eliminates the possiblity of more than a few hitting
+ * it within a certain time frame.
+ * 
+ * @author seanwagner
+ *
+ */
+public class ExecutorServiceDebouncer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ExecutorServiceDebouncer.class);
+	
+	private final Callback<Object, Object> callback;
+	private final long delay;
+	private final Object lock = new Object();
+    private final PushProvider<Boolean> shouldRun;
+    private final ScheduledThreadPoolExecutor pool;
+    private long time;
+    
+    private boolean signalOut;
+    
+    public enum RunType { RUN_BEFORE, RUN_AFTER };
+	private final RunType run;
+    
+	public ExecutorServiceDebouncer(
+        final Callback<Object, Object> callback,
+        final ScheduledThreadPoolExecutor executor,
+        final long delay,
+        final RunType run
+    ) {
+	    this(
+	        callback, 
+	        executor,
+	        delay, 
+	        new PushProvider<Boolean>() {
+
+                @Override
+                public boolean exists() throws ResourceException {
+                    return true;
+                }
+    
+                @Override
+                public Boolean provide() throws ResourceException {
+                    return true;
+                }
+
+                @Override
+                public void push(Boolean value) throws ResourceException {                    
+                }
+            },
+            run
+	    );
+	}
+	
+	public ExecutorServiceDebouncer(
+        final Callback<Object, Object> callback,
+        final ScheduledThreadPoolExecutor executor,
+        final Executor callbackThreads,
+        final long delay,
+        final RunType run
+    ) {
+        this(
+            callback, 
+            executor,
+            delay, 
+            new PushProvider<Boolean>() {
+
+                @Override
+                public boolean exists() throws ResourceException {
+                    return true;
+                }
+    
+                @Override
+                public Boolean provide() throws ResourceException {
+                    return true;
+                }
+
+                @Override
+                public void push(Boolean value) throws ResourceException {                    
+                }
+            },
+            run
+        );
+    }
+	
+	public ExecutorServiceDebouncer(
+	        final Callback<Object, Object> callback,
+	        final ScheduledThreadPoolExecutor executor,
+	        final long delay,
+	        final PushProvider<Boolean> shouldRun,
+	        final RunType run
+	    ) {
+	    
+	    this.shouldRun = shouldRun;
+		this.callback = callback;
+		this.delay = delay;
+		this.pool = executor;
+		this.run = run;
+		
+	}
+
+	/**
+	 * Signal the debouncer to execute.
+	 */
+	public synchronized void signal() {
+		
+		Runnable empty = new Runnable() {
+			@Override
+			public void run() {
+			
+			}
+		};
+		
+		Runnable runnable = new Runnable() {
+		       
+			@Override
+			public void run() {
+			    
+			    logger.debug("debouncer waiting " + System.currentTimeMillis());
+			    
+				synchronized(lock) {
+				    
+					if (run == RunType.RUN_AFTER) {
+						
+						if (pool.getActiveCount() == 1) {
+							
+					        try {
+					        	signalOut = false;
+					        	callback.call(null);
+	                        }
+	    				    catch (Exception e) {
+	                            logger.error("<Debouncer><1>, Debouncer interrupted:", e);
+	                        }
+					        
+						}
+						
+					}
+					
+					else if (run == RunType.RUN_BEFORE) {
+						
+						if (pool.getActiveCount() == 1) {
+							
+					        try {
+					        	signalOut = false;
+					        	callback.call(null);
+	                        }
+	    				    catch (Exception e) {
+	                            logger.error("<Debouncer><1>, Debouncer interrupted:", e);
+	                        }
+					        
+						}
+						
+					}
+				    
+				}
+				
+				logger.debug("debouncer not waiting" + System.currentTimeMillis());
+			    logger.debug("started debouncer");
+			    		
+			}
+			
+		};
+	
+	    logger.debug("debouncer signaled");
+	    
+		if(!signalOut) {
+		    
+		    logger.debug("signal wasn't out");
+		    
+		    try {
+		        
+		        synchronized(shouldRun) {
+		            
+                    if(shouldRun.provide()) {
+                        
+                        logger.debug("shouldRun");
+                        
+                        shouldRun.push(false);
+                        
+                    	signalOut = true;
+                    	
+                    	if (run == RunType.RUN_AFTER) {
+                    		pool.schedule(runnable, delay, TimeUnit.MILLISECONDS);
+                    	}
+                    	else if (run == RunType.RUN_BEFORE) {
+                    		
+                    		if (pool.getTaskCount() == 0) {
+                    			pool.schedule(runnable, 0, TimeUnit.MILLISECONDS);
+                    		} 
+                    		else {
+                    			pool.schedule(runnable, delay, TimeUnit.MILLISECONDS);
+                    		}
+                    		
+                    	}
+                    	
+                    }
+                    else {
+                        logger.debug("shouldn't run");
+                    }
+		        }
+                
+            }
+		    catch (ResourceException e) {
+                throw new RuntimeException("<Debouncer><2>, this shouldn't happen", e);
+            }
+		    
+		}
+		else {
+		    logger.debug("signal was out already doing nothing");
+		}
+		
+	}
+
+	/**
+	 * Force the debouncer to run.
+	 * 
+	 */
+    public void force() {
+        
+        synchronized(lock) {
+            lock.notifyAll();
+        }
+        
+    }
+    
+    public long timer() {
+    	return (System.currentTimeMillis() - time);
+    }
+
+    public void push(boolean b) {
+        
+        synchronized(shouldRun) {
+            
+            try {
+                shouldRun.push(b);
+            }
+            catch (ResourceException e) {
+                throw new RuntimeException("<Debouncer><3>, Debouncer failed to push. " + e.getMessage(), e);
+            }
+            
+        }
+        
+    }
+    /*
+     * Force the thread pool to stop, ending all current running threads
+     * 
+     */
+    public void shutdownNow() {
+    	pool.shutdownNow();
+    }
+    
+}
--- src/test/java/llc/ufwa/javacommon/test/concurrency/DebouncerTest.java
+++ src/test/java/llc/ufwa/javacommon/test/concurrency/DebouncerTest.java
@@ -1,11 +1,13 @@
 package llc.ufwa.javacommon.test.concurrency;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import junit.framework.TestCase;
 
 import llc.ufwa.concurrency.Callback;
 import llc.ufwa.concurrency.Debouncer;
+import llc.ufwa.concurrency.ExecutorServiceDebouncer;
 import llc.ufwa.concurrency.ParallelControl;
 
 import org.junit.Test;
@@ -16,10 +18,10 @@ public class DebouncerTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(DebouncerTest.class);
 	
-    @Test 
+    //@Test 
     public void testDebouncer() {
         
-    	for (int x = 0; x < 10; x++) {
+    	for (int x = 0; x < 5; x++) {
 	    	
 	        final ParallelControl<Integer> control = new ParallelControl<Integer>();
 	        
@@ -99,7 +101,7 @@ public class DebouncerTest {
         
     }
     
-    @Test 
+    //@Test 
     public void testDebouncerZeroTime() {
 
     	for (int x = 0; x < 20; x++) {
@@ -170,4 +172,353 @@ public class DebouncerTest {
     	}
         
     }
+    
+    @Test 
+    public void testExecutorServiceDebouncerZeroTime() {
+
+    	for (int x = 0; x < 20; x++) {
+    		
+	        final ParallelControl<Integer> control = new ParallelControl<Integer>();
+	        	        
+	        final ExecutorServiceDebouncer debouncer = new ExecutorServiceDebouncer(new Callback<Object, Object>() {
+	
+	            @Override
+	            public Object call(Object value) {
+	                
+	                Integer val = control.getValue();
+	                
+	                if(val == null) {
+	                    val = 1;
+	                }
+	                else {
+	                    val = val + 1;
+	                }
+	                
+	                control.setValue(val);
+	                
+	                logger.debug("set control value to " + val);
+	                
+	                return null;
+	                
+	            }}, new ScheduledThreadPoolExecutor(10), 1000, ExecutorServiceDebouncer.RunType.RUN_AFTER);
+	        
+	        debouncer.signal();
+	        debouncer.signal();
+	        debouncer.signal();
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(500);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        debouncer.signal();
+	        debouncer.signal();
+	        debouncer.signal();
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(500);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(500);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertNotSame((int)control.getValue(), 3); //this should be not the same because every debouncer.signal() call should run, 
+	        													//since there is no delay between them
+	        
+    	}
+        
+    }
+    
+    @Test 
+    public void testExecutorServiceDebouncerMultiThreaded() {
+        
+	    Runnable r = new Runnable() {
+	    	
+	        public void run() {
+	            
+	        	testExecutorServiceDebouncerRunAfter();
+	        	
+	        }
+	    };
+		
+	    for (int x = 0; x < 25; x++) {
+	    	new Thread(r).start();
+	    }
+    	
+    }
+    
+    
+    @Test 
+    public void testExecutorServiceDebouncerRunAfter() {
+        
+    	for (int x = 1; x < 5; x++) {
+    		
+	        final ParallelControl<Integer> control = new ParallelControl<Integer>();
+	        
+	        final ExecutorServiceDebouncer debouncer = new ExecutorServiceDebouncer(new Callback<Object, Object>() {
+	
+	            @Override
+	            public Object call(Object value) {
+	                
+	                Integer val = control.getValue();
+	                
+	                if(val == null) {
+	                    val = 1;
+	                }
+	                else {
+	                    val = val + 1;
+	                }
+	                
+	                control.setValue(val);
+	                
+	                logger.debug("set control value to " + val);
+	                
+	                return null;
+	                
+	            }}, new ScheduledThreadPoolExecutor(10), 700, ExecutorServiceDebouncer.RunType.RUN_AFTER);
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+
+	        for (int x1 = 0; x1 < 20; x1++) {
+	        	debouncer.signal();
+	        }
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+
+	        TestCase.assertEquals(1, (int)control.getValue());
+	        
+	        for (int x1 = 0; x1 < 10000; x1++) {
+	        	debouncer.signal();
+	        }
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+
+	        TestCase.assertEquals(2, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(3, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(4, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(5, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(6, (int)control.getValue());
+	        
+	        try {
+	            Thread.sleep(3000);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(6, (int)control.getValue());
+	        
+	        debouncer.shutdownNow();
+	        
+	        try {
+	            Thread.sleep(3000);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(6, (int)control.getValue());
+	        
+    	}
+        
+    }
+    
+    @Test 
+    public void testExecutorServiceDebouncerRunBefore() {
+        
+    	for (int x = 1; x < 5; x++) {
+	    	
+	        final ParallelControl<Integer> control = new ParallelControl<Integer>();
+	        
+	        final ExecutorServiceDebouncer debouncer = new ExecutorServiceDebouncer(new Callback<Object, Object>() {
+	
+	            @Override
+	            public Object call(Object value) {
+	                
+	                Integer val = control.getValue();
+	                
+	                if(val == null) {
+	                    val = 1;
+	                }
+	                else {
+	                    val = val + 1;
+	                }
+	                
+	                control.setValue(val);
+	                
+	                logger.debug("set control value to " + val);
+	                
+	                return null;
+	                
+	            }}, new ScheduledThreadPoolExecutor(10), 700, ExecutorServiceDebouncer.RunType.RUN_BEFORE);
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+
+	        for (int x1 = 0; x1 < 20; x1++) {
+	        	debouncer.signal();
+	        }
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+
+	        TestCase.assertEquals(1, (int)control.getValue());
+	        
+	        for (int x1 = 0; x1 < 10000; x1++) {
+	        	debouncer.signal();
+	        }
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+
+	        TestCase.assertEquals(2, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(3, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(4, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(5, (int)control.getValue());
+	        
+	        debouncer.signal();
+	        
+	        try {
+	            Thread.sleep(1100);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(6, (int)control.getValue());
+	        
+	        try {
+	            Thread.sleep(3000);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(6, (int)control.getValue());
+	        
+	        debouncer.shutdownNow();
+	        
+	        try {
+	            Thread.sleep(3000);
+	        } 
+	        catch (InterruptedException e) {
+	            TestCase.fail();
+	        }
+	        
+	        TestCase.assertEquals(6, (int)control.getValue());
+	        
+    	}
+        
+    }
+    
 }
