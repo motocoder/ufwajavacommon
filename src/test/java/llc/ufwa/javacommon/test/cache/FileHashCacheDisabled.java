1c08c8a53d55bf3a3f51bf141e80fa1a4df6d6d9
sean.robert.wagner <sean.robert.wagner@localhost>
2013-06-20 09:46:16 -0700

    disabled tests.

--- src/test/java/llc/ufwa/javacommon/test/cache/FileHashCacheDisabled.java
+++ src/test/java/llc/ufwa/javacommon/test/cache/FileHashCacheDisabled.java
@@ -12,76 +12,19 @@ import java.util.Random;
 
 import junit.framework.TestCase;
 import llc.ufwa.data.exception.ResourceException;
-import llc.ufwa.data.resource.cache.Cache;
-import llc.ufwa.data.resource.cache.FileCache;
-import llc.ufwa.data.resource.cache.FilePersistedExpiringCache;
+import llc.ufwa.data.resource.cache.FileHashCache;
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class FilePersistedExpiringCacheTest {
+public class FileHashCacheDisabled {
 
-    private static final Logger logger = LoggerFactory.getLogger(FilePersistedExpiringCacheTest.class);
-
-//    @Test
-//    public void testExpiringPart() {
-//    	
-//    	try {
-//    		
-//	    	final long timeout = 1000;
-//			
-//	        final File dataFolder = new File("./target/test-files/temp");
-//	        final File tempFolder = new File("./target/test-files/temp/data");
-//	        
-//	        deleteRoot(tempFolder);
-//	
-//	        final File internalFile = new File(tempFolder, "ad-properties"); 
-//	        final File rootFile = new File(dataFolder, "ad-properties");
-//	        
-//			Cache<String, InputStream> internal = new FileCache(internalFile, -1L, timeout);
-//			Cache<String, InputStream> InpersistingRoot = new FileCache(rootFile, -1L, timeout);
-//	
-//			final long cleanupTimeout = 1500;
-//			
-//			final Cache<String, InputStream> cache = new FilePersistedExpiringCache(internal, InpersistingRoot, timeout, cleanupTimeout);
-//			
-//			final String key = "dfsa";
-//	        final String value = "dfsadsf";
-//	        final String key2 = "fgdd";
-//	        final String value2 = "dfgsds";
-//	        String returnValue;
-//	        
-//	        // TEST PUT, GET, REMOVE, and EXISTS
-//	        
-//	        cache.put(key, new ByteArrayInputStream(value.getBytes()));
-//	        
-//	        InputStream baos = cache.get(key);
-//			
-//	        returnValue = getStringFromInputStream(baos);
-//	        
-//	        TestCase.assertEquals(value, returnValue);
-//	        TestCase.assertEquals(cache.exists(key), true);
-//
-//	        Thread.sleep(timeout+100);
-//	        
-//	        TestCase.assertEquals(cache.exists(key), false); //this should be false because it's after the timeout time
-//			
-//		}
-//		catch (ResourceException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//    	
-//    }
-//    
+    private static final Logger logger = LoggerFactory.getLogger(FileHashCacheDisabled.class);
+	
 //	@Test 
-//    public void testFilePersistedExpiringCacheTest() {
+//    public void testFileHashCache() {
 //		
 //		for (int x = 0; x < 10; x++) {
 //			universalTest();
@@ -90,14 +33,16 @@ public class FilePersistedExpiringCacheTest {
 //	}
 //	
 //	@Test 
-//    public void testFilePersistedExpiringCacheTestMultiThreaded() {
+//    public void testFileHashCacheMultiThreaded() {
 //		
 //		for (int x = 0; x < 15; x++) {
 //			 new Thread() {
 //	                
 //	                @Override
 //	                public void run() {
-//	                	universalTest();
+//	                	
+//            			universalTest();
+//	                	
 //	                }
 //	                
 //			 }.start();
@@ -106,30 +51,21 @@ public class FilePersistedExpiringCacheTest {
 //	}
 //	
 //	public void universalTest() {
-//
+//		
 //		try {
-//			
-//			final long timeout = 1000;
 //			
 //			Random random = new Random();
 //			int suffix = random.nextInt(4000);
 //			
-//	        final File dataFolder = new File("./target/test-files/temp");
-//	        final File tempFolder = new File("./target/test-files/temp/data" + String.valueOf(suffix));
+//	        final File tempFolder = new File("./target/test-files/temp-data" + String.valueOf(suffix));
+//	        final File dataFolder = new File("./target/test-files/temp-data/data" + String.valueOf(suffix));
 //	        
 //	        deleteRoot(tempFolder);
-//	
-//	        final File internalFile = new File(tempFolder, "ad-properties"); 
-//	        final File rootFile = new File(dataFolder, "ad-properties");
+//	        deleteRoot(dataFolder);
 //	        
-//			Cache<String, InputStream> internal = new FileCache(internalFile, -1L, timeout);
-//			Cache<String, InputStream> InpersistingRoot = new FileCache(rootFile, -1L, timeout);
-//	
-//			final long cleanupTimeout = 1500;
-//			
-//			final Cache<String, InputStream> cache = new FilePersistedExpiringCache(internal, InpersistingRoot, timeout, cleanupTimeout);
-//			
-//			final String key = "dfsa";
+//	        final FileHashCache cache = new FileHashCache(dataFolder, tempFolder);
+//	        
+//	        final String key = "dfsa";
 //	        final String value = "dfsadsf";
 //	        final String key2 = "fgdd";
 //	        final String value2 = "dfgsds";
@@ -159,7 +95,7 @@ public class FilePersistedExpiringCacheTest {
 //	        
 //	        cache.put(key, new ByteArrayInputStream(value.getBytes()));
 //	        cache.put(key2, new ByteArrayInputStream(value2.getBytes()));
-//	
+//
 //	        TestCase.assertEquals(cache.exists(key), true);
 //	        TestCase.assertEquals(cache.exists(key2), true);
 //	        
@@ -180,7 +116,7 @@ public class FilePersistedExpiringCacheTest {
 //		       
 //	        TestCase.assertEquals(cache.exists(key), false);
 //	        TestCase.assertEquals(cache.exists(key2), false);
-//        
+//	        
 //		}
 //		catch (ResourceException e) {
 //			// TODO Auto-generated catch block
@@ -218,7 +154,7 @@ public class FilePersistedExpiringCacheTest {
 //		return sb.toString();
 // 
 //	}
-//
+//	
 //	void deleteRoot (File root) {
 //        if (root.exists()) {
 //            
@@ -230,5 +166,5 @@ public class FilePersistedExpiringCacheTest {
 //            root.delete();
 //        }
 //    }
-	
+
 }
