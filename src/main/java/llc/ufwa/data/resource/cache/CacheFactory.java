99315a4e97233076757f2317b9151bec0059bdcb
sean.robert.wagner <sean.robert.wagner@localhost>
2013-09-04 15:22:24 -0700

    cache factory fix.

--- src/main/java/llc/ufwa/data/resource/cache/CacheFactory.java
+++ src/main/java/llc/ufwa/data/resource/cache/CacheFactory.java
@@ -67,7 +67,6 @@ public class CacheFactory {
     public static final <Value> Cache<String, Value> getSerializingMaxCountFileCache(
         final int maxCount,
         final File cacheRoot,
-        final Converter<Integer, Value> sizeConverter,
         final Callback<Void, Value> onRemoved
     ) {
         
